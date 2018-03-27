af97410631454e11d96beb752a73b4db75a5821b
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-27 13:42:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(songTitle, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist
 }
 
