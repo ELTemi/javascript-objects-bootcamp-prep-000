254bd9af9d7a1554e60704052f3c829576830f45
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-27 13:36:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { artistNames: "songTitles" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
 
